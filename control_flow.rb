e9812dac6461b47d5f3e0ee2dd3d1e0d88da3d4a
jay <jaypifiverr@gmail.com>
2022-12-15 01:18:04 -0500

    commit tree 758a638aa889deb31c231bd08d5963691367f586

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,48 @@
 def admin_login(username, password)
-  # your code here
+  if username = "admin" && password = "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  reply = if temperature < 40
+    "brisk"
+  elsif temperature >= 40
+    "a little chilly"
+  elsif temperature > 85
+    "too dang hot"
+  elsif 
+    "perfect"
+  end
+  "It's #{reply} out there!"
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else
+    return num 
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
